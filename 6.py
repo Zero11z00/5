905a3c1a194415d607e2076c00f65e734b0e7c8c
Zero11z00 <74456752+Zero11z00@users.noreply.github.com>
2020-11-18 18:38:00 +0300

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,88 @@
+import os
+os.system ("clear")
+
+print ("hi")
+
+print ("This is the termex commands for beginners+Tool calls for the group Amino")
+
+print ("Joker.a.1.3")
+
+cod = input ("Update: ")
+
+if cod ==  "apt update && apt upgrade" :
+
+ print ("apt update && apt upgrade")
+
+print("typ'apt update && apt upgrade' ")
+
+input ("==>:")
+
+cod = input ("terms of reference: ")
+
+if cod ==  "termux-setup-storage" :
+
+ print ("termux-setup-storage")
+
+print("typ'termux-setup-storage' ")
+
+input ("==>:")
+
+cod = input ("cod: ")
+
+if cod ==  "apt install git" :
+
+ print ("apt install git")
+
+print("typ'apt install git' ")
+
+input ("==>:")
+
+cod = input ("cod: ")
+
+if cod ==  "apt install python" :
+
+ print ("apt install python")
+
+print("typ'apt install python' ")
+
+input ("==>:")
+
+cod = input ("cod: ")
+
+if cod ==  "pip install Amino.py" :
+
+ print ("pip install Amino.py")
+
+print("typ'pip install Amino.py' ")
+
+input ("==>:")
+
+cod = input ("cod: ")
+
+if cod ==  "git clone https://github.com/kira-xc/AminoInvite" :
+
+ print ("git clone https://github.com/kira-xc/AminoInvite")
+
+print("typ'git clone https://github.com/kira-xc/AminoInvite' ")
+
+input ("==>:")
+
+cod = input ("running: ")
+
+if cod ==  "cd AminoInvite" :
+
+ print ("cd AminoInvite")
+
+print("typ'cd AminoInvite' ")
+
+input ("==>:")
+
+cod = input ("running: ")
+
+if cod ==  "python invite.py" :
+
+ print ("python invite.py")
+
+print("typ'python invite.py' ")
+
+input ("==>:")
